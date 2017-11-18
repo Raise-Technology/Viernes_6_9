7dc0ba224e5c6f586eb76ff4414fa5811d7d398d
Cesar <Cesar@192.168.1.73>
2017-11-17 21:14:08 -0600

    Primer cambios aceptados con el proyecto GIT.

---
+++ src/proyectogit/ProyectoGIT.java
@@ -0,0 +1,14 @@
+package proyectogit;
+
+/**
+ *
+ * @author Gilberto Zazueta
+ */
+public class ProyectoGIT {
+    
+    public static void main(String[] args) {
+        Principal P = new Principal();
+        P.setVisible(true);
+    }
+    
+}
